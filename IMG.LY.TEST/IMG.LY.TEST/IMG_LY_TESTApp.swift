b815b3fb89637c644b3bead733a1606224c83798
Miguel Bou Sleiman <mbousleiman@hotmail.com>
2025-02-19 14:40:17 +0100

    Created project

---
+++ IMG.LY.TEST/IMG.LY.TEST/IMG_LY_TESTApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMG_LY_TESTApp.swift
+//  IMG.LY.TEST
+//
+//  Created by Miguel Bou Sleiman on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct IMG_LY_TESTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IMG.LY.TEST/IMG.LY.TESTTests/IMG_LY_TESTTests.swift
@@ -0,0 +1,16 @@
+//
+//  IMG_LY_TESTTests.swift
+//  IMG.LY.TESTTests
+//
+//  Created by Miguel Bou Sleiman on 2/19/25.
+//
+
+import Testing
+
+struct IMG_LY_TESTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
