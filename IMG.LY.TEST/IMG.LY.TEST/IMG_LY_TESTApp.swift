8fbab1c27ae99bbf566773d2cf8b4825c3cac87a
Miguel Bou Sleiman <mbousleiman@hotmail.com>
2025-02-23 17:44:03 +0100

    Created SPM packages

--- IMG.LY.TEST/IMG.LY.TEST/IMG_LY_TESTApp.swift
+++ IMG.LY.TEST/IMG.LY.TEST/IMG_LY_TESTApp.swift
@@ -5,13 +5,13 @@
 //  Created by Miguel Bou Sleiman on 2/19/25.
 //
 
-import SwiftUI
-
-@main
-struct IMG_LY_TESTApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
-}
+//import SwiftUI
+//
+//@main
+//struct IMG_LY_TESTApp: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//        }
+//    }
+//}
